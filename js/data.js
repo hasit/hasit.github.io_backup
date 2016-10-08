763b93eb08837cc9fa491e4d8e276a0e59d76262
Hasit Mistry <hasitnm@gmail.com>
2016-10-08 15:09:54 -0700

    Fetch data from /data folder and render it to on the page.

---
+++ js/data.js
@@ -0,0 +1,46 @@
+(function () {
+  window.addEventListener('load', function () {
+    events();
+    expandGroups('click');
+    expandGroups('touchend');
+  }, false);
+})();
+
+function expandGroups(event) {
+  var els = document.getElementsByClassName('title');
+  Array.prototype.forEach.call(els, function (el) {
+    el.addEventListener(event, function () {
+      el.parentElement.classList.toggle('expand');
+    }, false);
+  });
+}
+
+// build headers for requests
+function buildHeader() {
+  var head = new Headers();
+  head.append('pragma', 'no-cache');
+  head.append('cache-control', 'no-cache');
+
+  var init = {
+    method: 'GET',
+    headers: head,
+  };
+  return init;
+}
+
+// pull highlights content
+function pullEvents() {
+  return fetch('./data/events.json', buildHeader()).then(function (response) {
+    return response.json();
+  });
+}
+
+function events() {
+  var h = document.getElementById('events');
+  pullEvents().then(function (r) {
+    for (var i = 0, len = r.data.length; i < len; i++) {
+      var x = r.data[i];
+      h.innerHTML += '<li><div class="date">' + x.date + '</div><div class="desc"><div class="what">' + x.description.what + '</div><div class="info">' + x.description.info + '</div></div></li>';
+    }
+  });
+}
